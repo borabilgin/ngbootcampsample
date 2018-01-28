a55ec475914ca06f0d6f8ab1cc0190ff795dad50
Jaime Pokhoy <jaimepokhoy@gmail.com>
2018-01-28 02:20:09 -0500

    Create Components
    Initialize basic components, create home page and initial lists of lists

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'list/:id', component: ListComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule
   ],
   providers: [],
